2892b7d1631e9804af302990fc97336810714a26
alitokur <alitokurr@gmail.com>
2022-12-25 06:40:36 -0800

    sorry prime i ll use gruvbox for now :space_invader

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -12,17 +12,9 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
-
+  use { "ellisonleao/gruvbox.nvim" }
   use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
+  --use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
