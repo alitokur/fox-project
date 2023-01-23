1af17147bf03888f7a40ad5df17d01c2a5ac43ab
badcode <alitokurr@gmail.com>
2023-01-23 18:29:05 +0300

    Update treesitter.lua

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "c", "cpp", "cmake", "javascript", "json", "vim", "comment", "markdown", "lua" },
+  ensure_installed = { "help", "c", "cpp", "cmake", "make", "javascript", "json", "vim", "comment", "markdown", "lua" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
