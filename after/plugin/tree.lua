a042f040322898ca9333614570ce7d888ba89798
alitokur <alitokurr@gmail.com>
2023-02-09 11:20:24 -0800

    fix map-lua toggle issue

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -9,13 +9,24 @@ vim.opt.termguicolors = true
 --require("nvim-tree").setup()
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
 
-local function open_nvim_tree()
+local function open_nvim_tree(data)
+  local is_real_file = vim.fn.filereadable(data.file) == 1
+  local is_no_name_file = data.file == "" and vim.bo[data.buf].buftype == ""
+  local is_a_directory = vim.fn.isdirectory(data.file) == 1
 
-  -- open the tree
-  require("nvim-tree.api").tree.open()
+  if is_a_directory then
+    vim.cmd.cd(data.file)
+    require("nvim-tree.api").tree.open()
+    return
+  end
+
+  if is_real_file or is_no_name_file then
+    require("nvim-tree.api").tree.toggle { focus = false, find_file = true }
+    return
+  end
 end
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 local HEIGHT_RATIO = 0.8  -- You can change this
 local WIDTH_RATIO = 0.5   -- You can change this too
