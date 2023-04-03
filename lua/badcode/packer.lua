9b77509fd95aadefc4e203d811f479ca6ac8c24c
alitokur <alitokurr@gmail.com>
2023-04-03 14:20:04 -0700

    add toggleterm.nvim :cat2:

--- lua/badcode/packer.lua
+++ lua/badcode/packer.lua
@@ -75,6 +75,11 @@ return require('packer').startup(function(use)
     use {
         'lewis6991/gitsigns.nvim',
     }
+    
+    --toggleterm
+    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+        require("toggleterm").setup()
+    end}
 
 end)
 
