f36f89c32504afccb9474b78b3f9fbdb4946d82c
alitokur <alitokurr@gmail.com>
2023-06-23 18:47:06 +0300

    [theme] update color theme :fox_face:

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,1 +1,9 @@
-vim.cmd("colorscheme onedark")
+vim.cmd [[set background=dark]]
+vim.cmd [[set termguicolors]]
+vim.cmd[[let g:everforest_background = 'hard']]
+vim.cmd[[let g:everforest_better_performance = 1]]
+vim.cmd[[let g:everforest_ui_contrast = 'high']]
+vim.cmd [[colorscheme everforest]]
+
+
+
