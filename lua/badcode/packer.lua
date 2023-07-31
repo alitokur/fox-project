4d7d359c9523ecc9afe44a08e92d344b673aa5a1
badcode <alitokurr@gmail.com>
2023-07-31 21:59:27 +0300

    [feat]: Add auto-load support to packer.lua (Issue #6)
    - Add ensure_packer function to check if packer.nvim is installed and install it if not
    - Minor plugin updates

--- lua/badcode/packer.lua
+++ lua/badcode/packer.lua
@@ -1,15 +1,45 @@
-vim.cmd.packadd('packer.nvim')
-return require('packer').startup(function(use)
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+local packer_bootstrap = ensure_packer()
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
+-- Have packer use a popup window
+packer.init({
+    display = {
+        open_fn = function()
+            return require('packer.util').float({ border = 'single' })
+        end
+    }
+})
+
+-- vim.cmd.packadd('packer.nvim')
+return packer.startup(function(use)
     -- plugin manager
     use 'wbthomason/packer.nvim'
     -- fuzzy finder
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
     -- theme
-    use ({ 'sainnhe/everforest' })
+    -- use ({ 'sainnhe/everforest' })
+    use({
+        "neanias/everforest-nvim",
+        -- Optional; default configuration will be used if setup isn't called.
+    }) 
     --treesitter
     use {
         'nvim-treesitter/nvim-treesitter',
@@ -25,6 +55,7 @@ return require('packer').startup(function(use)
     use ( 'mbbill/undotree' )
     --indent blankline
     use ('lukas-reineke/indent-blankline.nvim')
+    --lsp 
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
@@ -38,7 +69,6 @@ return require('packer').startup(function(use)
             end,
         },
         {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
         -- Autocompletion
         {'hrsh7th/nvim-cmp'},     -- Required
         {'hrsh7th/cmp-nvim-lsp'}, -- Required
@@ -51,89 +81,87 @@ return require('packer').startup(function(use)
         {"onsails/lspkind-nvim"},
     }
 }
+--file browser
+use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+        'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+}
+--airline
+use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+}
+--comment
+use {
+    'numToStr/Comment.nvim',
+    config = function()
+        require('Comment').setup()
+    end
+}
+--gitsigns.nvim
+use {
+    'lewis6991/gitsigns.nvim',
+}
+--toggleterm
+use {
+    "akinsho/toggleterm.nvim", tag = '*',
+}
+--symbols-outline
+use {
+    "simrat39/symbols-outline.nvim"
+}
+--barbar
+use {'romgrk/barbar.nvim', requires = 'nvim-web-devicons'}
+--barbecue
+use({
+    "utilyre/barbecue.nvim",
+    tag = "*",
+    requires = {
+        "SmiteshP/nvim-navic",
+    },
+    after = "nvim-web-devicons", -- keep this if you're using NvChad
+    config = function()
+        require("barbecue").setup()
+    end,
+})
+--scroolbar
+use("petertriho/nvim-scrollbar")
+use {
+    "startup-nvim/startup.nvim",
+    requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+    config = function()
+        require"startup".setup()
+    end
+}
+use {"ecthelionvi/NeoColumn.nvim"}
 
+use {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+}
 
-    --file browser
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional, for file icons
-        },
-        tag = 'nightly' -- optional, updated every week. (see issue #1193)
-    }
-    --airline
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-    --comment
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
-    --gitsigns.nvim
-    use {
-        'lewis6991/gitsigns.nvim',
-    }
-    --toggleterm
-    use {
-        "akinsho/toggleterm.nvim", tag = '*',
-    }
-    --symbols-outline
-    use {
-        "simrat39/symbols-outline.nvim"
-    }
-    --barbar
-    use {'romgrk/barbar.nvim', requires = 'nvim-web-devicons'}
-    --barbecue
-    use({
-        "utilyre/barbecue.nvim",
-        tag = "*",
-        requires = {
-            "SmiteshP/nvim-navic",
-        },
-        after = "nvim-web-devicons", -- keep this if you're using NvChad
-        config = function()
-            require("barbecue").setup()
-        end,
-    })
-    --scroolbar
-    use("petertriho/nvim-scrollbar")
-    use {
-        "startup-nvim/startup.nvim",
-        requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
-        config = function()
-            require"startup".setup()
-        end
-    }
-    use {"ecthelionvi/NeoColumn.nvim"}
+use {
+    'norcalli/nvim-colorizer.lua'
+}
 
-    use {
-        'phaazon/hop.nvim',
-        branch = 'v2', -- optional but strongly recommended
-    }
+use {
+    'folke/todo-comments.nvim'
+}
 
-    use {
-        'norcalli/nvim-colorizer.lua'
-    }
+use{
+    "folke/trouble.nvim"
+}
 
-    use {
-        'folke/todo-comments.nvim'
-    }
+use 'folke/lsp-colors.nvim'
 
-    use{
-        "folke/trouble.nvim"
-    }
-
-    use 'folke/lsp-colors.nvim'
-
-    use {
-  'rmagatti/goto-preview',
-  config = function()
-    require('goto-preview').setup {}
-  end
+use {
+    'rmagatti/goto-preview',
+    config = function()
+        require('goto-preview').setup {}
+    end
 }
 
 -- nvim v0.7.2
@@ -145,5 +173,8 @@ use({
     },
 })
 
+if packer_bootstrap then
+    require('packer').sync()
+end
 
 end)
