3e10cd96334889b0b93b92b12a2f0b0162e6c6d1
alitokur <ali@armadast.com>
2024-10-31 18:12:01 +0300

    feat: add session manager

--- lua/badcode/packer.lua
+++ lua/badcode/packer.lua
@@ -184,6 +184,20 @@ use { 'VonHeikemen/fine-cmdline.nvim', requires = {
 }
 use {"github/copilot.vim"}
 
+use { 'RRethy/vim-illuminate' }
+
+use {
+    'rmagatti/auto-session',
+    config = function()
+        require("auto-session").setup {
+            log_level = "error",
+            auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+        }
+    end
+}
+
+use {'nvim-telescope/telescope-ui-select.nvim' }
+
 if packer_bootstrap then
     require('packer').sync()
 end
