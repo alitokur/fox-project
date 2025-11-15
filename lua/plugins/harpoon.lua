f21a95eb2d5a1b64405588bf930fd3a75981e3ac
badcode <alitokurr@gmail.com>
2025-11-15 20:52:36 +0300

    minor fixes

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,14 +12,14 @@ return {
   keys = function()
     local keys = {
       {
-        "<leader>H",
+        "<leader>e",
         function()
           require("harpoon"):list():add()
         end,
         desc = "Harpoon File",
       },
       {
-        "<leader>h",
+        "<leader>a",
         function()
           local harpoon = require("harpoon")
           harpoon.ui:toggle_quick_menu(harpoon:list())
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -32,7 +32,7 @@ return {
     { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
     { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
     { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
-    { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+    -- { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
     -- find
     { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
