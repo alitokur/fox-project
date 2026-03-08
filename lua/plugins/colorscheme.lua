2f461d593e7f79213a3165c9cbd440558fe875b8
badcode <alitokurr@gmail.com>
2026-03-08 22:37:05 +0300

    we are back to dracula bois

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,88 +1,43 @@
--- -- Add this to your Lazy plugin list
--- return {
---   "rebelot/kanagawa.nvim",
---   priority = 1000, -- load before all other plugins
---   config = function()
---     require("kanagawa").setup({
---       compile = false,
---       undercurl = true,
---       commentStyle = { italic = true },
---       functionStyle = {},
---       keywordStyle = { italic = true },
---       statementStyle = { bold = true },
---       typeStyle = {},
---       transparent = false,
---       dimInactive = false,
---       terminalColors = true,
---       colors = {
---                   theme = {
---         all = {
---             ui = {
---                 bg_gutter = "none"
---             }
---         }
---     }
---       },
---       overrides = function(colors)
---         return {}
---       end,
---       theme = "wave",
---       background = {
---         dark = "wave",
---         light = "lotus"
---       },
---     })
---
---     -- Apply the colorscheme
---     vim.cmd("colorscheme kanagawa-dragon")
---   end,
--- }
-
--- return {
---   "vague-theme/vague.nvim",
---   lazy = false, -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other plugins
---   config = function()
---     -- NOTE: you do not need to call setup if you don't want to.
---     require("vague").setup({
---       -- optional configuration here
---     })
---     vim.cmd("colorscheme vague")
---   end
--- }
---
-
--- return {
---   'everviolet/nvim', name = 'evergarden',
---   priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
---   opts = {
---     theme = {
---       variant = 'fall', -- 'winter'|'fall'|'spring'|'summer'
---       accent = 'green',
---     },
---     editor = {
---       transparent_background = false,
---       sign = { color = 'none' },
---       float = {
---         color = 'mantle',
---         solid_border = false,
---       },
---       completion = {
---         color = 'surface0',
---       },
---     },
---   }
--- }
---
 return {
-    "AlexvZyl/nordic.nvim",
-    lazy = false,
+  {
+    "Mofiqul/dracula.nvim",
+    name = "dracula",
     priority = 1000,
     config = function()
-        require("nordic").setup({
-            transparent = { bg = true, float = true, }
-        })
 
-        require("nordic").load()
-    end,
+      require("dracula").setup({
+        italic_comment = true,
+        transparent_bg = false,
+      })
+
+      vim.cmd.colorscheme("dracula")
+
+      -- darker background
+      local bg = "#181818"
+
+      local hl = vim.api.nvim_set_hl
+
+      hl(0,"Normal",{bg=bg})
+      hl(0,"NormalNC",{bg=bg})
+      hl(0,"SignColumn",{bg=bg})
+      hl(0,"EndOfBuffer",{bg=bg})
+      hl(0,"NormalFloat",{bg=bg})
+      hl(0,"FloatBorder",{bg=bg})
+
+      hl(0,"CursorLine",{bg="#2a2c3a"})
+      hl(0,"ColorColumn",{bg="#2b2d3a"})
+      hl(0,"LineNr",{fg="#6272a4"})
+      hl(0,"CursorLineNr",{fg="#f8f8f2",bold=true})
+
+      -- better C++ syntax visibility
+      hl(0,"@type",{fg="#8be9fd"})
+      hl(0,"@function",{fg="#50fa7b"})
+      hl(0,"@constructor",{fg="#8be9fd"})
+      hl(0,"@keyword",{fg="#ff79c6"})
+      hl(0,"@parameter",{fg="#ffb86c"})
+      hl(0,"@field",{fg="#bd93f9"})
+      hl(0,"@comment",{fg="#6272a4",italic=true})
+
+    end
+  }
 }
