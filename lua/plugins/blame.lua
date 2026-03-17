1e6e8b265170b23572a211a12f4092d481ef3c6e
alitokur <alitokurr@gmail.com>
2026-03-17 13:07:29 +0300

    minor fix

--- lua/plugins/blame.lua
+++ lua/plugins/blame.lua
@@ -7,12 +7,11 @@ return {
     -- If you want to load the plugin at startup, add something like event = "VeryLazy",
     -- or lazy = false. One of both options will work.
     opts = {
-        -- your configuration comes here
-        -- for example
         enabled = true,  -- if you want to enable the plugin
         message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
         date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
         virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+        --timeout
     },
  
 }
